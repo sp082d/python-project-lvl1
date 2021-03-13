1f39afaf6764c1d4b83375bb252084915b4c89e9
Philipp <sp082d@gmail.com>
2021-03-13 12:44:58 +0300

    PyCharm recomendations

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -1,12 +1,15 @@
 import prompt 
 
+
 def welcome_user():
     print("Welcome to the Brain Games!")
     name = prompt.string('May I have your name? ')
     print("Hello, " + name + "!")
 
+
 def main():
     welcome_user()
+
 
 if __name__ == '__main__':
     main()
