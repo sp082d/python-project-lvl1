5b61a5ca3779287ab9f985335588639568b4714b
Philipp <sp082d@gmail.com>
2021-03-13 12:13:07 +0300

    add welcome_user()

---
+++ brain_games/cli.py
@@ -0,0 +1,13 @@
+import prompt 
+
+def welcome_user():
+    print("Welcome to the Brain Games!")
+    name = prompt.string('May I have your name? ')
+    print("Hello, " + name + "!")
+
+def main():
+    welcome_user()
+
+if __name__ == '__main__':
+    main()
+
