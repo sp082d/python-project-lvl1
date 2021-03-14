f4573136fce42dd41f780c526f7b347d05117a98
Philipp <sp082d@gmail.com>
2021-03-14 08:46:21 +0300

    do rithgt way print f-string

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -4,7 +4,7 @@ import prompt
 def welcome_user():
     print("Welcome to the Brain Games!")
     name = prompt.string('May I have your name? ')
-    print("Hello, " + name + "!")
+    print(f'Hello, {name}!')
 
 
 def main():
@@ -13,4 +13,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
