1e48cc183141bc69b543bc5242eadf394933b5b4
Philipp <sp082d@gmail.com>
2021-03-22 10:48:19 +0300

    add main and if __name__

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -1,29 +1,38 @@
 import random
 import prompt
 
-print("Welcome to the Brain Games!")
-name = prompt.string('May I have your name? ')
-print(f'Hello, {name}!')
 
-print('Answer "yes" if the number is even, otherwise answer "no".')
+def brain_even():
+    print("Welcome to the Brain Games!")
+    name = prompt.string('May I have your name? ')
+    print(f'Hello, {name}!')
 
-k = 0
+    print('Answer "yes" if the number is even, otherwise answer "no".')
 
-while k < 3:
-    number = random.randint(5, 30)
-    parity = 'no' if number % 2 else 'yes'
-    stack = (number, parity)
+    k = 0
 
-    print('Question: ', number)
-    answer = prompt.string('Your answer: ')
-    print(stack)
+    while k < 3:
+        number = random.randint(5, 30)
+        parity = 'no' if number % 2 else 'yes'
+        stack = (number, parity)
 
-    if answer == parity:
-        print('Correct!')
-        k += 1
+        print('Question: ', number)
+        answer = prompt.string('Your answer: ')
+        print(stack)
+
+        if answer == parity:
+            print('Correct!')
+            k += 1
+        else:
+            print(f"Let's try again, {name}")
+            break
     else:
-        print(f"Let's try again, {name}")
-        break
-else:
-    print(f'Congratulations, {name}!')
+        print(f'Congratulations, {name}!')
 
+
+def main():
+    brain_even()
+
+
+if __name__ == '__main__':
+    main()
