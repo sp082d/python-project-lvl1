c5ae21cbddcfb49900b3c41c97b8aef787d9c2da
Philipp <sp082d@gmail.com>
2021-03-22 11:35:49 +0300

    fill brain_cals

--- brain_games/scripts/brain_calc.py
+++ brain_games/scripts/brain_calc.py
@@ -1,6 +1,7 @@
 import random
 import prompt
 
+
 def brain_calc():
     print("Welcome to the Brain Games!")
     name = prompt.string('May I have your name? ')
@@ -8,17 +9,30 @@ def brain_calc():
 
     print('What is the result of the expression?')
 
+    operations = ['+', '-', '*']
+
     while True:
-        number = random.randint(1, 10)
-        operations = ['+', '-', '*']
+        f_number = str(random.randint(1, 10))
+        s_number = str(random.randint(1, 10))
         rand_operation = random.choice(operations)
 
-        print(rand_operation)
+        expr = f_number + rand_operation + s_number
+        val_expr = str(eval(expr))
+        stack = (expr, val_expr)
 
+        print('Question: ', expr)
+        answer = prompt.string('Your answer: ')
+        print(stack)
+
+        if answer == val_expr:
+            print('Correct!')
+        else:
+            print(f" '{answer}' is wrong answer;(. Correct answer was '{val_expr}'.")
+            print(f"Let's try again, {name}!")
+            break
 
 
 def main():
-
     brain_calc()
 
 
