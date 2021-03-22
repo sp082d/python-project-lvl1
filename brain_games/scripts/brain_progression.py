3567ffce740cfb0af0802e289e62d335d49781ca
Philipp <sp082d@gmail.com>
2021-03-22 13:37:13 +0300

    all fill brain_progression.py

--- brain_games/scripts/brain_progression.py
+++ brain_games/scripts/brain_progression.py
@@ -9,13 +9,25 @@ def brain_progression():
 
     print('What number is missing in the progression?')
 
-    # while True:
-    start_seq = random.randint(1, 10)
-    end_seq = random.randint(11, 20)
+    while True:
+        start_seq = random.randint(1, 5)
+        end_seq = random.randint(6, 13)
+        seq = list(range(start_seq, end_seq))
+        new_seq = seq.copy()
 
-    seq = range(start_seq, end_seq)
+        rand_pos = random.randint(0, len(seq) - 1)
+        elem = str(seq[rand_pos])
+        new_seq[rand_pos] = ".."
 
-    print(seq)
+        print(f'Question: {new_seq}')
+        answer = prompt.string('Your answer: ')
+
+        if elem == answer:
+            print('Correct!')
+        else:
+            print(f" '{answer}' is wrong answer;(. Correct answer was '{seq[rand_pos]}'.")
+            print(f"Let's try again, {name}!")
+            break
 
 
 def main():
