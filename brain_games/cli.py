91c267553bfaee670522d03497b4fe64acb9c13a
sp082d <43670149+sp082d@users.noreply.github.com>
2021-11-15 10:48:43 +0300

    Rewrite cli.py welcome_user to get_user_name() and get_user_answer()

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -5,8 +5,13 @@
 import prompt
 
 
-def welcome_user():
-    """Requests a username and returns a greeting."""
-    username = prompt.string('May I have your name? ')
-    greeting = 'Hello, {}!'.format(username)
-    return greeting
+def get_user_name():
+    """Prompt user for his/her name."""
+    user_name = prompt.string('May I have your name? ')
+    return user_name
+
+
+def get_user_answer():
+    """Prompt user for answer."""
+    answer = prompt.string('Your answer: ')
+    return answer
