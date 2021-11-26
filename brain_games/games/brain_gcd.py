dae863d32ed3ff4c81eb13e5b6963849d90e270a
sp082d <sp082d@gmail.com>
2021-11-26 09:57:11 +0300

    Refactor brain_gcd.py and add gcd original function

--- brain_games/games/brain_gcd.py
+++ brain_games/games/brain_gcd.py
@@ -3,7 +3,6 @@
 """Brain GCD game functions."""
 
 
-import math
 from brain_games.game_engine import generate_number
 
 
@@ -12,5 +11,13 @@ def make_question():
     num1 = generate_number()
     num2 = generate_number()
     question = f'Question: {num1} {num2}'
-    answer = math.gcd(num1, num2)
+    answer = gcd(num1, num2)
     return question, str(answer)
+
+
+def gcd(num1, num2):
+    """Return GCD of two numbers."""
+    if not num2:
+        return num1
+    else:
+        return gcd(num2, num1 % num2)
